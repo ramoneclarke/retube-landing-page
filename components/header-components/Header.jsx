042a2c02ef2f1058e1eb8c4497bf70698c68d65b
Ramone Clarke <ramonejlclarke@gmail.com>
2023-05-23 17:42:14 +0100

    implement header

---
+++ components/header-components/Header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Logo from "./Logo";
+
+const Header = () => {
+  const router = useRouter();
+
+  return (
+    <div className="w-4/5 h-20 mb-20 justify-between flex items-center">
+      <Logo />
+      <div className="flex gap-12 flex-1 pl-24">
+        <button className="text-gray-500 font-medium hover:text-mid">
+          Features
+        </button>
+        <button className="text-gray-500 font-medium hover:text-mid">
+          Pricing
+        </button>
+      </div>
+      <div className="flex gap-6">
+        <button
+          onClick={() =>
+            router.push(`${process.env.NEXT_PUBLIC_APP_DOMAIN}/login`)
+          }
+          className="text-darkest font-medium hover:text-mid"
+        >
+          Log in
+        </button>
+        <button
+          onClick={() =>
+            router.push(`${process.env.NEXT_PUBLIC_APP_DOMAIN}/signup`)
+          }
+          className="bg-brand text-darkest rounded-2xl hover:opacity-80 font-semibold px-8 py-4"
+        >
+          Sign up
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ components/header-components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="flex w-fit flex-row items-center ">
+      <div className="text-3xl font-extrabold">
+        <span className="text-darkest dark:text-lightest">Re</span>
+        <span className="text-brand">Tube</span>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
